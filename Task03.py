9dced202da15765fb6aa930ffac17408a36ceb54
Igor <garkol@bk.ru>
2022-09-30 23:37:32 +0300

    Task03

--- Task03.py
+++ Task03.py
@@ -1,6 +1,10 @@
 # Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
 # Пример: Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
+from multiprocessing.dummy import Value
+
+
 n = int(input("Введите число: "))
-value = {n: (1 + (1 / n)) ** n for n in range(1, n + 1)}
-print(value)
+V = {n: round((1 + (1 / n)) ** n) for n in range(1, n + 1)}
+print(V)
+print(sum(V.values()))
